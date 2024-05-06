bc9b2a3843e40dad84bab69cb99dac24e3fb5a4c
Tulasi2304 <tulasi.udupa23@gmail.com>
2024-05-06 21:53:31 +0530

    Connect to MongoDb

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const ejs = require('ejs');
-const day = require('./date.js');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -8,12 +8,28 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-const items = [];
-const workItems = [];
+mongoose.connect('mongodb://localhost:27017/todolistDB');
+
+const itemSchema = new mongoose.Schema({
+    name: String
+});
+
+const Item = mongoose.model('Item', itemSchema);
+
+const item1 = new Item({
+    name: 'Welcome to your ToDo List'
+});
+
+const defaultItem = [item1];
+
+Item.insertMany(defaultItem).then(() => {
+    console.log("Successfully inserted default items");
+}).catch((error) => {
+    console.log(error);
+});
 
 app.get("/", (req, res) =>{
-    const getDay = day();
-    res.render('list', {title: getDay, newItems: items});
+    res.render('list', {title: "Today", newItems: items});
 });
 
 app.post("/", (req, res) => {
